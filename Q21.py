f467c9b981bccd54148dfc66006d6d8442811cf8
Cadyn Reger <116835253+creger01@users.noreply.github.com>
2022-12-06 13:35:28 -0500

    Add files via upload

---
+++ Q21.py
@@ -0,0 +1,6 @@
+# the authors names are: Cadyn Reger
+def rectangle_area(x,y):
+    area = x * y
+    print("The area of the rectangle is", area)
+
+rectangle_area(12,10)
---
+++ Q22.py
@@ -0,0 +1,7 @@
+#the authors names are: Cadyn Reger
+import math
+def new_function(a,b,c):
+    y = 2*math.pi*(a)**3+3*math.sin(b)+530.27*math.sqrt(c+34)
+    print("y =", y)
+
+new_function(20,0,30)
